33ff1da3c8a5d9a5cbfac5c3bfa209833bcc05fa
Filipe Campos <felipetwo@gmail.com>
2021-10-28 07:26:29 -0300

    Primeira Classe: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,42 @@
+package boardgame;
+
+public class Position {
+	
+	private int row;
+	private int colum;
+	
+	public Position() {
+		
+	}
+
+	public Position(int row, int colum) {
+		this.row = row;
+		this.colum = colum;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColum() {
+		return colum;
+	}
+
+	public void setColum(int colum) {
+		this.colum = colum;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(row);
+		builder.append(", ");
+		builder.append(colum);
+		return builder.toString();
+	}
+
+}
