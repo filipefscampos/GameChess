6378c3381387f218063828d595de2aa9fbf9ca24
Filipe Campos <felipetwo@gmail.com>
2021-12-26 20:17:20 -0300

    Possibilidades de Mover uma Peca

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -16,4 +16,10 @@ public class King extends ChessPiece {
 		builder.append("K");
 		return builder.toString();
 	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 }
