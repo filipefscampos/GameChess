ad28423715079a487261b90634633e0af9e983dd
Filipe Campos <felipetwo@gmail.com>
2021-12-26 18:57:30 -0300

    Movendo Pecas

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Scanner;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
@@ -10,13 +12,25 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		
+		while(true) {
 		UI.printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.print("Origem: ");
+		ChessPosition source = UI.readChessPosition(sc);
+		
+		System.out.println();
+		System.out.print("Destino: ");
+		ChessPosition target = UI.readChessPosition(sc);
+		
+		ChessPiece capturedPiece  = chessMatch.performChessMove(source, target);
+		}
 		
 		
 		
 		
 		
-		sc.close();
+
 
 	}
 
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -44,6 +44,19 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+	
+	public Piece removePiece (Position position) {
+		if(!positionExits(position)) {
+			throw new BoardException("Posição não existe no Board");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;		
+	}
 
 	private boolean positionExits(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
